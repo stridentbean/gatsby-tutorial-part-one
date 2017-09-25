b781cb898fbd00323371c9741d75718497854889
Michael Borglin <Borglin.Me@gmail.com>
2017-09-25 14:09:15 -0700

    Completed first tutorial.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello gatsby world!</div>
+export default () =>
+  <div style={{ color: 'tomato' }}>
+    <h1>Hello gatsby world!</h1>
+    <p>What a world!</p>
+    <img src="http://lorempixel.com/400/200/" alt="" />
+    <br />
+    <Link to="/page-2/">Linkage</Link>
+    <br />
+    <Link to="/counter/">Counter</Link>
+  </div>
